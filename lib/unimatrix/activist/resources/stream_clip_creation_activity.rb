f7123f20fe9d8456a5c9114049fd4993cdc986cd
jacksonsouza <jacksonsouza@me.com>
2017-05-09 15:45:46 -0700

    name changes

--- lib/unimatrix/activist/resources/stream_clip_creation_activity.rb
+++ lib/unimatrix/activist/resources/stream_clip_creation_activity.rb
@@ -4,10 +4,8 @@ module Unimatrix::Activist
     field       :in_point
     field       :out_point
     field       :original_stream_id
-    field       :assembled_weaver_url
     field       :video_file_url
-    field       :video_binary_id
-    field       :clip_name
+    field       :video_id
   end
 
 end
--- lib/unimatrix/activist/resources/video_ingestion_task.rb
+++ lib/unimatrix/activist/resources/video_ingestion_task.rb
@@ -1,10 +1,9 @@
 module Unimatrix::Activist
 
   class VideoIngestionTask < Task
-    field       :assembled_weaver_url
+    field       :video_id
     field       :video_file_url
     field       :video_binary_id
-    field       :clip_name
   end
 
 end
