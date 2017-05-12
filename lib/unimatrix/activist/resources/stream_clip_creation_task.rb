aa27b26cb4e2fae61813e426e25046ffe5925d5a
jacksonsouza <jacksonsouza@me.com>
2017-05-12 15:15:09 -0700

    add clip name

--- lib/unimatrix/activist/resources/stream_clip_creation_task.rb
+++ lib/unimatrix/activist/resources/stream_clip_creation_task.rb
@@ -2,6 +2,7 @@ module Unimatrix::Activist
 
   class StreamClipCreationTask < Task
     field       :in_point
+    field       :clip_name
     field       :video_file_url
     field       :out_point
     field       :original_stream_id
--- lib/unimatrix/activist/resources/video_ingestion_task.rb
+++ lib/unimatrix/activist/resources/video_ingestion_task.rb
@@ -2,6 +2,7 @@ module Unimatrix::Activist
 
   class VideoIngestionTask < Task
     field       :video_id
+    field       :clip_name
     field       :video_file_url
   end
 
